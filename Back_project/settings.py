703595b7536dd55baf52bd1b0cda83794118e606
Kenji Ozawa <keoz002@ess.ais.ac.nz>
2025-03-10 15:40:17 +1300

    Init 2

--- Back_project/settings.py
+++ Back_project/settings.py
@@ -130,4 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CORS_ORIGIN_ALLOW_ALL = True
