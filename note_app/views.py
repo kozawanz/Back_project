2d83480578173b7f2b86ea83e1ac94692c0ca3ac
Kenji Ozawa <keoz002@ess.ais.ac.nz>
2025-03-17 14:00:49 +1300

    created registration api

---
+++ note_app/views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from note_app.serializers import UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
